cc0ff15bb2b4f6fabc50cd7c8bb247674e1f5706
dvinhliu <luuducvinh1392@gmail.com>
2024-07-06 21:41:09 +0700

    MaxShop

---
+++ admin/TestMailer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mailer</title>
+</head>
+<body>
+    <form action="send_email.php" method="post"> <!-- Specify action and method -->
+        <input type="submit" name="send" value="Send Email">
+    </form>
+</body>
+</html>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,113 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+   <!-- Sidebar - Brand -->
+   <a class="sidebar-brand d-flex align-items-center justify-content-center" href="Home.php">
+      <div class="sidebar-brand-icon rotate-n-15">
+         <i class="fas fa-laugh-wink"></i>
+      </div>
+      <div class="sidebar-brand-text mx-3">MAXSHOP</div>
+   </a>
+   <!-- Divider -->
+   <hr class="sidebar-divider my-0">
+   <!-- Nav Item - Dashboard -->
+   <li class="nav-item active">
+      <a class="nav-link" href="Home.php">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span></a>
+   </li>
+   <!-- Divider -->
+   <hr class="sidebar-divider">
+   <!-- Heading -->
+   <div class="sidebar-heading">
+      Chức năng chính
+   </div>
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+         aria-expanded="true" aria-controls="collapseTwo">
+      <i class="fa-solid fa-list-ul"></i>
+      <span>Thương hiệu</span>
+      </a>
+      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+         <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="ListBrands.php">Danh sách thương hiệu</a>
+            <a class="collapse-item" href="AddBrands.php">Thêm thương hiệu</a>
+         </div>
+      </div>
+   </li>
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree"
+         aria-expanded="true" aria-controls="collapseThree">
+      <i class="fa-brands fa-product-hunt"></i>
+      <span>Sản phẩm</span>
+      </a>
+      <div id="collapseThree" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+         <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Custom Components:</h6>
+            <a class="collapse-item" href="ListProducts.php">Danh sách sản phẩm</a>
+            <a class="collapse-item" href="AddProducts.php">Thêm sản phẩm</a>
+         </div>
+      </div>
+   </li>
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+      <a class="nav-link collapsed" href="ListOrder.php" aria-expanded="true">
+         <i class="fa-solid fa-wallet"></i>
+         <span>Đơn hàng</span>
+      </a>
+   </li>
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+      <a class="nav-link collapsed" href="ListCustomers.php" aria-expanded="true">
+         <i class="fa-solid fa-users"></i>
+         <span>Người dùng</span>
+      </a>
+   </li>
+   <!-- Divider -->
+   <hr class="sidebar-divider">
+   <!-- Heading -->
+   <div class="sidebar-heading">
+      Addons
+   </div>
+   <!-- Nav Item - Pages Collapse Menu -->
+   <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+         aria-expanded="true" aria-controls="collapsePages">
+      <i class="fas fa-fw fa-folder"></i>
+      <span>Pages</span>
+      </a>
+      <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+         <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Login Screens:</h6>
+            <a class="collapse-item" href="login.html">Login</a>
+            <a class="collapse-item" href="register.html">Register</a>
+            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+            <div class="collapse-divider"></div>
+            <h6 class="collapse-header">Other Pages:</h6>
+            <a class="collapse-item" href="404.html">404 Page</a>
+            <a class="collapse-item" href="blank.html">Blank Page</a>
+         </div>
+      </div>
+   </li>
+   <!-- Nav Item - Charts -->
+   <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+      <i class="fas fa-fw fa-chart-area"></i>
+      <span>Charts</span></a>
+   </li>
+   <!-- Nav Item - Tables -->
+   <li class="nav-item">
+      <a class="nav-link" href="tables.html">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Tables</span></a>
+   </li>
+   <!-- Divider -->
+   <hr class="sidebar-divider d-none d-md-block">
+   <!-- Sidebar Toggler (Sidebar) -->
+   <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+   </div>
+</ul>
+<!-- End of Sidebar -->
